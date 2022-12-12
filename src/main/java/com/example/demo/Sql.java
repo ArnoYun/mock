4db4a3d08a6e942d1cd01b39de67c861068e1dd4
Akmal <akmal199818@gmail.com>
2022-12-12 15:51:49 +0500

    class SQL added

---
+++ src/main/java/com/example/demo/Sql.java
@@ -0,0 +1,78 @@
+package com.example.demo;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Sql {
+    private static final String DB_USERNAME = "admin";
+    private static final String DB_PASSWORD = "admin";
+    private static final String DB_URL = "jdbc:postgresql://192.168.56.102:5432/users";
+
+    public static ArrayList<String> select(String login) throws SQLException {
+
+            ArrayList <String> arr = new ArrayList<>();
+            Connection connection = null;
+            Statement statement = null;
+            ResultSet result = null;
+            try{connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+                statement = connection.createStatement();
+                String SELECT_LOGIN = "select * from users.login_time where login ='" + login +"'";
+                result = statement.executeQuery(SELECT_LOGIN);
+                ResultSetMetaData resultMeta = result.getMetaData();
+                while(result.next()){
+                    for(int i = 1;i<= resultMeta.getColumnCount();i++){
+                        arr.add(result.getString(i));
+                    }
+                    }
+                } catch (SQLException e) {
+                throw e;
+            }
+            finally {
+                assert connection != null;
+                connection.close();
+                assert statement != null;
+                statement.close();
+                assert result != null;
+                result.close();
+            }
+        return arr;
+        }
+
+    public static int insert(String insert_login, String insert_password, String insert_name) {
+
+        int exception = 0;
+        try (Connection connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD)){;
+            String insert_1 = "insert into users.logins (login, password, name) values (?,?,?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(insert_1);
+            preparedStatement.setString(1, insert_login);
+            preparedStatement.setString(2, insert_password);
+            preparedStatement.setString(3, insert_name);
+            preparedStatement.executeQuery();
+        }
+        catch (SQLException ex) {
+            if(ex.getMessage().equals("Запрос не вернул результатов.")){
+                exception = 1;
+            }
+        }
+        return exception;
+    }
+    public static void insert_time(String insert_login,String time)  {
+
+        try(Connection connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD)) {;
+            String insert = "insert into users.login_time (login, login_time) values (?,?)";
+            PreparedStatement preparedStatement = connection.prepareStatement(insert);
+            preparedStatement.setString(1, insert_login);
+            preparedStatement.setString(2, time);
+            preparedStatement.executeQuery();
+        }
+        catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
+
+    }
+
+
+
+
--- src/main/java/com/example/demo/controller/MockController.java
+++ src/main/java/com/example/demo/controller/MockController.java
@@ -1,34 +1,68 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.apache.catalina.connector.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.sql.SQLDataException;
+import java.sql.SQLException;
 import java.text.SimpleDateFormat;
 import java.util.*;
+
+import static com.example.demo.Sql.*;
 
 @RestController
 @RequestMapping
 public class MockController {
 
-    @GetMapping(value = "get")
-    public List<Map<String, String>> list(){
-        return new ArrayList<Map<String, String>>() {{
-            add(new HashMap<String, String>() {{put("text", "Hello World"); }});
-        }};
+    @GetMapping(value = "get_login/{login}")
+    public List<Map<String, String>> list(String get, @PathVariable String login)  {
 
+        try {
+            ArrayList<String> get_result = select(login);
+            System.out.println(get_result);
+            String get_login = get_result.get(0);
+            String get_time = get_result.get(1);
+            return new ArrayList<Map<String, String>>() {{
+                add(new HashMap<String, String>() {{
+                    put("login", get_login);
+                    put("time", get_time);
+                }});
+            }};
+        }
+        catch(Exception e){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
     }
-    @PostMapping(value = "login")
-    public List<Map<String, String>> authorization (@RequestBody Map<String, String> credentials){
-        String name = credentials.get("username");
-        SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss");
-        Date date = new Date(System.currentTimeMillis());
-        String time = format.format(date);
-        return new ArrayList<Map<String, String>>() {{
-            add(new HashMap<String, String>() {{put("time", time);put("login", name);}});
-        }};
-    }
+
+
+        @PostMapping(value = "login")
+        public List<Map<String, String>> authorization (@RequestBody Map<String, String> credentials) {
+            String login = credentials.get("login");
+            String password = credentials.get("password");
+            String name = credentials.get("name");
+            SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss");
+            Date date = new Date(System.currentTimeMillis());
+            String time = format.format(date);
+            try {
+                if(insert(login, password, name) == 1){
+                    insert_time(login, time);
+                    return new ArrayList<Map<String, String>>() {{
+                        add(new HashMap<String, String>() {{
+                            put("time", time);
+                            put("login", login);
+                        }});
+                    }};
+                }
+                else {
+                    throw new Exception() ;
+                }
+            }
+            catch (Exception e){
+                throw new ResponseStatusException(HttpStatus.METHOD_NOT_ALLOWED);
+            }
+        }
 
 }
